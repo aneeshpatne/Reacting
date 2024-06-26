261b36966bcd23e4b5f8bef297b3b53004051012
aneeshpatne <aneeshpatne@gmail.com>
2024-06-26 16:42:27 +0530

    fetching from local api

---
+++ 2/1_revise/myapp/src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function App(){
+  const [value, valueSetter] = useState('')
+  async function helper(){
+    try{
+    const res = await fetch('http://127.0.0.1:5000');
+    const resp = await res.json();
+    console.log(resp) ;
+    valueSetter(resp) ;
+    }
+    catch(error){
+      console.error(error);
+    }
+  }
+return (<div>
+  {value}
+  <h1>Hello</h1>
+  <button onClick={helper}>Click Here</button>
+  </div>);
+}
+
+export default App ; 
