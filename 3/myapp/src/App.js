223ca668f4a4f7522199b0bb620170f90acc3d38
aneeshpatne <aneeshpatne@gmail.com>
2024-07-01 17:45:00 +0530

    props, inc bit of complexity

--- 3/myapp/src/App.js
+++ 3/myapp/src/App.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import './App.css';
 import { coffee } from './assets/img';
-function Coffee({ name }) {
+function Coffee({ name,imgsrc }) {
   return (
     <div className='main-box'>
       <h2 className='coffee-name'>Coffee Name: {name}</h2>
       <div className='button-container'>
         <button className='b1'>Buy Now</button>
+      </div>
+      <div className='img-container'>
+        <img src={imgsrc}></img>
       </div>
     </div>
   );
@@ -14,7 +17,7 @@ function Coffee({ name }) {
 function App(){
   //const [count,setCount] = useState(1);
   return(<div>
-  <Coffee name='Espresso'/>
+  <Coffee name='Espresso' imgsrc={coffee}/>
   <Coffee name='Americano'/>
   <Coffee name='Latte'/>
   </div>
