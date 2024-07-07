2b609303e1940856cbdb543996bf40d6462e1609
aneeshpatne <aneeshpatne@gmail.com>
2024-07-07 19:24:56 +0530

    conditional rendering

--- 5 - Media Query and mics/my-app/src/App.js
+++ 5 - Media Query and mics/my-app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import ListRender from './components/listRender';
+import ConRen from './components/conditionalRendering';
 const items = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
 function App() {
   return (<div>
     {items.map((item,index) => <ListRender items ={item} key={index}/>)}
+    <ConRen />
   </div>
     
 
---
+++ 5 - Media Query and mics/my-app/src/components/conditionalRendering.jsx
@@ -0,0 +1,7 @@
+const Hello = true;
+function ConRen(){
+    return(<div>
+        {Hello && <p>True</p>}
+    </div>);
+}
+export default ConRen;
