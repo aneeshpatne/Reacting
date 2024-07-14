1b802beb34ad96d6ab29903a4ef1cc45d2d9b90d
aneeshpatne <aneeshpatne@gmail.com>
2024-07-14 19:38:31 +0530

    list styling

--- 6 - More interactivity/my-app/src/App.js
+++ 6 - More interactivity/my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import SkillData from './components/data';
 //import MidLayout from './components/midLayout';
 import Project from './components/projects';
 function App() {
   return (
     <div>
     <Project />
+    <SkillData />
     </div>
   );
 }
